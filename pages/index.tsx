4446c1a4d95e58a6aee1d66f2c147bb42bfe303c
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-21 14:10:46 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,7 +67,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
              <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
              "If it can be imagned, we can make it"
           </p>
-          
+          <div className="max-w-[60ch] columns-2 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
              href="mailto:craig@inoculate.media?cc=terbo@inoculate.media&subject=The%20subject%20of%20the%20email">
@@ -79,6 +79,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               Tools
             </a>
           </div>
+              </div>
           {images.map(({ id, public_id, format, blurDataUrl }) => (
             <Link
               key={id}
