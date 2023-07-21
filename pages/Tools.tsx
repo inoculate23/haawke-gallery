f849b9a4070fb4d2be1e61efde869090c940be46
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-21 12:18:36 -0700

    Create Tools.tsx

---
+++ pages/Tools.tsx
@@ -0,0 +1,93 @@
+iport type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect, useRef } from 'react'
+
+import Logo from '../components/Icons/Logo'
+import Modal from '../components/Modal'
+import cloudinary from '../utils/cloudinary'
+import getBase64ImageUrl from '../utils/generateBlurPlaceholder'
+import type { ImageProps } from '../utils/types'
+import { useLastViewedPhoto } from '../utils/useLastViewedPhoto'
+
+const Tools: NextPage = ({ iframe }) => {
+  const <iframe src="https://editor.fusionbrain.ai/" frameborder="0" class="src"></iframe> 
+  const { iframe } = router.query
+ 
+  
+
+  useEffect(() => {
+    // This effect keeps track of the last viewed photo in the modal to keep the index page in sync when the user navigates back
+    if (lastViewedPhoto && !photoId) {
+      lastViewedPhotoRef.current.scrollIntoView({ block: 'center' })
+      setLastViewedPhoto(null)
+    }
+  }, [photoId, lastViewedPhoto, setLastViewedPhoto])
+
+  return (
+    <>
+      <Head>
+        <title>Haawke Neural Technology - Las Vegas AI experts</title>
+        <meta
+          property="og:image"
+          content="https://nextjsconf-pics.vercel.app/og-image.png"
+        />
+        <meta
+          name="twitter:image"
+          content="https://nextjsconf-pics.vercel.app/og-image.png"
+        />
+      </Head>
+      <main className="mx-auto max-w-[1960px] p-4">
+        {iframe}
+          />
+        )}
+       
+            
+          </div>
+         
+      </main>
+      <footer className="p-6 text-center text-white/80 sm:p-12">
+        Thank you to{' '}
+        <a
+          href="https://edelsonphotography.com/"
+          target="_blank"
+          className="font-semibold hover:text-white"
+          rel="noreferrer"
+        >
+          Josh Edelson
+        </a>
+        ,{' '}
+        <a
+          href="https://www.newrevmedia.com/"
+          target="_blank"
+          className="font-semibold hover:text-white"
+          rel="noreferrer"
+        >
+          Jenny Morgan
+        </a>
+        , and{' '}
+        <a
+          href="https://www.garysextonphotography.com/"
+          target="_blank"
+          className="font-semibold hover:text-white"
+          rel="noreferrer"
+        >
+          Gary Sexton
+        </a>{' '}
+        for the pictures.
+      </footer>
+    </>
+  
+export default Tools
+
+export async function getStaticProps() {
+  const results = await iframe
+   
+  return {
+    props: {
+      images: reducedResults,
+    },
+  }
+}
