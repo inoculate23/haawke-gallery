4bd4a61f2d1a2c1e7c8814c59e466b427ab92613
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-21 13:52:27 -0700

    Update Tools.tsx

--- pages/Tools.tsx
+++ pages/Tools.tsx
@@ -12,37 +12,23 @@ import getBase64ImageUrl from '../utils/generateBlurPlaceholder'
 import type { ImageProps } from '../utils/types'
 import { useLastViewedPhoto } from '../utils/useLastViewedPhoto'
 
+function MyPage() {
   return (
     <>
       <Head>
-        <title>Haawke Neural Technology - Las Vegas AI experts</title>
-        <meta
-          property="og:image"
-          content="https://nextjsconf-pics.vercel.app/og-image.png"
-        />
-        <meta
-          name="twitter:image"
-          content="https://nextjsconf-pics.vercel.app/og-image.png"
-        />
+        <title>Kandinsky 2.2</title>
       </Head>
-      <main className="mx-auto max-w-[1960px] p-4">
-
-            <iframe src="https://editor.fusionbrain.ai/"
-            id="myFrame"
-            width="100%" 
-             height="100%
-          </iframe>
-     )
-          />
-        )}
-      
-         
-      </main>
-  
+      <h1>My Page</h1>
+      <iframe src="https://editor.fusionbrain.ai/" width="1920" height="1080"></iframe>
     </>
-  )
+  );
 }
-export default Tools
+
+
+
+            
+ 
+export default Tools;
 
 export async function getStaticProps() {
   const results = await iframe
