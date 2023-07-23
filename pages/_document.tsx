7cbbeb009816b47cd0f269a952fff6fb24df30c0
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-23 10:28:25 -0700

    Add files via upload

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ import {
   BsTwitter,
 } from "react-icons/bs";
 import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
+import sidebar from "../components/sidebar";
 import { SidebarProvider } from "./context/SidebarContext";
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 
