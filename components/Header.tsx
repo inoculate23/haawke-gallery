0dffd1bf4c1081e334573901635d5d5006678b9d
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-23 19:35:19 -0700

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { FC } from "react";
 import { useSidebarContext } from "../context/SidebarContext";
 
-const Header: FC<Record<string, never>> = function () {
+const header: FC<Record<string, never>> = function () {
   const { isOpenOnSmallScreens, isPageWithSidebar, setOpenOnSmallScreens } =
     useSidebarContext();
 
