0552fcc445447a054415777cf9296a3b71869a64
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-29 16:34:14 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,9 +44,34 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
           content="https://nextjsconf-pics.vercel.app/og-image.png"
         />
       </Head>
-
+  <NavDropdown>
+      <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
+         <Navbar expand="lg" bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">Haawke Neural Teachnology</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">AI Tools</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      </NavDropdown>
       <main className="mx-auto max-w-[1960px] p-4">
-   <NavDropdown/>
+ 
         {photoId && (
           <Modal
             images={images}
