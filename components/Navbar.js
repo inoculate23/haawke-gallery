d10fb878e88bb203d8167f958b2c9198c0893f0b
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-29 16:43:18 -0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavDropdownExample() {
+function NavDropdown() {
   const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
 
   return (
