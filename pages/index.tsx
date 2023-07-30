5fac3696cb129dfa4118d12f3afb914a26280e11
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-30 06:04:32 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,16 +40,11 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
  
       <Head data-bs-theme="dark">
         <title>Haawke Neural Technology - Las Vegas AI experts</title>
-        <meta
-          property="og:image"
-          content="https://nextjsconf-pics.vercel.app/og-image.png"
+        <MetaProperty="og:image" content="https://nextjsconf-pics.vercel.app/og-image.png"
         />
-        <meta
-          name="twitter:image"
-          content="https://nextjsconf-pics.vercel.app/og-image.png"
+        <MetaName="twitter:image" content="https://nextjsconf-pics.vercel.app/og-image.png"
         />
-        <link
-  rel="stylesheet"
+        <link rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
   integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
   crossOrigin="anonymous"
