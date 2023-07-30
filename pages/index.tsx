74db987d32af3285c33de114918cf57846ea3349
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-29 17:53:12 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,8 +64,8 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             />{' '}
            Haawke Neural Technology
           </Navbar.Brand>
-      <ButtonGroup className="mb-2" variant="dark">
-        <Button href="https://huggingface.co/HaawkeNeural">Huggingface</Button>
+      <ButtonGroup className="mb-2">
+        <Button href="https://huggingface.co/HaawkeNeural" variant="dark">Huggingface</Button>
         <Button href="./tools">AI tools</Button>
        <Button href="mailto:craig@inoculate.media?cc=terbo@inoculate.media&subject=The%20subject%20of%20the%20email">Contact</Button>
              <Button href="https://github.com/inoculate23">Github</Button>
