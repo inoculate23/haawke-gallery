8427266727a76d5fdcee46490eeaa13112c7621b
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-30 05:27:51 -0700

    Update BasicButton.js

--- components/BasicButton.js
+++ components/BasicButton.js
@@ -5,8 +5,11 @@ function BasicButton() {
   return (
     <DropdownButton id="dropdown-basic-button" title="AI Tools">
       <Dropdown.Item href="Tools">Fusion Brain</Dropdown.Item>
-      <Dropdown.Item href="https://inpainter-stable-diffusion-beige.vercel.app/">Inpaintern</Dropdown.Item>
+      <Dropdown.Item href="https://inpainter-stable-diffusion-beige.vercel.app/">Inpainter</Dropdown.Item>
       <Dropdown.Item href="https://scribble-diffusion-ivory-six.vercel.app/">Scribble Diffusion</Dropdown.Item>
+   <Dropdown.Item href="https://paint-by-text-tau.vercel.app/">Text-to-paint</Dropdown.Item>
+       <Dropdown.Item href="https://next-stable-diffusion-replicate-text2img.vercel.app/">Stable Diffusion</Dropdown.Item>
+   <Dropdown.Item href="https://ai-code-translator2-ochre.vercel.app/">Code Translator</Dropdown.Item>
     </DropdownButton>
   );
 }
