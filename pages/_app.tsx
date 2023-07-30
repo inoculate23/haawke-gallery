fce3e4f91c2d96d4cd79cfcde8b5186292439ef1
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-29 18:57:51 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,40 +7,7 @@ function App() {
 	return (
 		<div>
 			<div style={{ textAlign: "center" }}>
-				<h1 className="text-green text-3xl font-bold">
-					GeeksforGeeks
-				</h1>
-				<h3 className="text-black dark:text-white text-2xl">
-					Adding Dark Mode in ReactJS using Tailwind CSS
-				</h3>
-			</div>
-		
-				<Switcher />
-				<div className="w-56 overflow-hidden bg-white
-				rounded-lg border border-gray-200
-				shadow-md dark:bg-gray-800 dark:border-gray-700">
-					<img
-						className="rounded-t-lg"
-						src=
-"https://media.geeksforgeeks.org/wp-content/uploads/20220221132017/download.png"
-						alt="gfg"
-					/>
-					<div className="p-5">
-						<a href="##">
-							<h5 className="mb-2 text-2xl
-							font-bold tracking-tight
-							text-gray-900 dark:text-white">
-								GeeksforGeeks
-							</h5>
-						</a>
-						<p className="mb-3 font-normal text-gray-700
-							dark:text-gray-400">
-							
-						</p>
-					</div>
-				</div>
-			
-		</div>
+				
 	);
 }
 
