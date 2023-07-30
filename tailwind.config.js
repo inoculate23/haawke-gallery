6dd916bb989ceb5747b4a5dfeb005e814b4704ec
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-29 17:57:56 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
     extend: {
       boxShadow: {
         highlight: 'inset 0 0 0 1px rgba(255, 255, 255, 0.05)',
+         darkMode: "class",
       },
       screens: {
         narrow: { raw: '(max-aspect-ratio: 3 / 2)' },
