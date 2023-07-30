12d54287ad901c98a07eeed39a44ea84d0f6aea8
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-30 05:39:46 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -70,10 +70,13 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
           </Navbar.Brand>
       <ButtonGroup className="mb-2">
         <Button href="https://huggingface.co/HaawkeNeural">Huggingface</Button>
-           <DropdownButton id="dropdown-basic-button" title="AI Tools">
+             <DropdownButton id="dropdown-basic-button" title="AI Tools">
       <Dropdown.Item href="Tools">Fusion Brain</Dropdown.Item>
-      <Dropdown.Item href="https://inpainter-stable-diffusion-beige.vercel.app/">Inpaintern</Dropdown.Item>
+      <Dropdown.Item href="https://inpainter-stable-diffusion-beige.vercel.app/">Inpainter</Dropdown.Item>
       <Dropdown.Item href="https://scribble-diffusion-ivory-six.vercel.app/">Scribble Diffusion</Dropdown.Item>
+   <Dropdown.Item href="https://paint-by-text-tau.vercel.app/">Text-to-paint</Dropdown.Item>
+       <Dropdown.Item href="https://next-stable-diffusion-replicate-text2img.vercel.app/">Stable Diffusion</Dropdown.Item>
+   <Dropdown.Item href="https://ai-code-translator2-ochre.vercel.app/">Code Translator</Dropdown.Item>
     </DropdownButton>
        <Button href="mailto:craig@inoculate.media?cc=terbo@inoculate.media&subject=The%20subject%20of%20the%20email">Contact</Button>
              <Button href="https://github.com/inoculate23">Github</Button>
