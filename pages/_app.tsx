28b56af40d427fbd0e3a06bf422363d5e1c41261
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-29 18:15:35 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,7 +27,7 @@ function App() {
 					/>
 					<div className="p-5">
 						<a href="##">
-							<h5 class="mb-2 text-2xl
+							<h5 className="mb-2 text-2xl
 							font-bold tracking-tight
 							text-gray-900 dark:text-white">
 								GeeksforGeeks
