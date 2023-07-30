5115a1d9ffb0ec256df7f18c799e497ca054e0fa
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-07-29 17:36:56 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,6 +45,12 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
           name="twitter:image"
           content="https://nextjsconf-pics.vercel.app/og-image.png"
         />
+        <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+  integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+  crossorigin="anonymous"
+/>
       </Head>
  <Navbar bg="dark" data-bs-theme="dark">
   
