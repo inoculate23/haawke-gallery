5c394da1e986292d344cef5a56246eddc7ce630f
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-08-03 23:31:24 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import BasicButton from '../components/BasicButton';
   import Dropdown from 'react-bootstrap/Dropdown';
-import OgImage from '../public/og-image';
+import OgImage from '../public/og-image.png';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   const router = useRouter()
@@ -44,7 +44,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         <title>Haawke Neural Technology - Las Vegas AI experts</title>
         <meta
           property="og:image"
-          content={lOgImage}"
+          content={OgImage}"
         />
         <meta
           name="twitter:image"
