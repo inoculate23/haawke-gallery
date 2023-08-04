699798a6f34297daa73fca162b7fa2f3bcf37900
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-08-04 02:38:17 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,14 +39,16 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   return (
  <>
       <Head data-bs-theme="dark">
-       
+       </>
+   <>
         <link rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
   integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
   crossOrigin="anonymous"
 />
-    
+    </>
       </Head>
+ 
  <Navbar bg="dark" data-bs-theme="dark">
   
           <Navbar.Brand href="#home">
