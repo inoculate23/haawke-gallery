21a9affcaa643198de526e2c94f91e07b182f10a
Craig Ellenwood <73420772+inoculate23@users.noreply.github.com>
2023-08-03 23:35:55 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,6 @@ import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import BasicButton from '../components/BasicButton';
   import Dropdown from 'react-bootstrap/Dropdown';
-import OgImage from '../public/og-image.png';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   const router = useRouter()
@@ -39,27 +38,19 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
 
   return (
  
-      <Head>
-  
+      <Head data-bs-theme="dark">
         <title>Haawke Neural Technology - Las Vegas AI experts</title>
-        <meta
-          property="og:image"
-          content={OgImage}"
+        <MetaProperty="og:image" content="https://nextjsconf-pics.vercel.app/og-image.png"
         />
-        <meta
-          name="twitter:image"
-          content="https://nextjsconf-pics.vercel.app/og-image.png"
+        <MetaName="twitter:image" content="https://nextjsconf-pics.vercel.app/og-image.png"
         />
-                <link rel="stylesheet"
+        <link rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
   integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
   crossOrigin="anonymous"
 />
-           
+    
       </Head>
-
-   
-   
  <Navbar bg="dark" data-bs-theme="dark">
   
           <Navbar.Brand href="#home">
@@ -162,7 +153,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
     
   )
 }
-      </Fragment>
+
   
 export default Home
 
@@ -203,5 +194,4 @@ export async function getStaticProps() {
     },
   }
 }
-
 
